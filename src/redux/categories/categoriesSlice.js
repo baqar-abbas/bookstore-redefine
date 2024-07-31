d477c3695eab2ba483054ea561e51ee3c18b520b
baqar-abbas <baqar007@hotmail.com>
2024-07-31 14:14:23 +0500

    Setup Store, Slices, reducers and actions for the React App.

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    categories: [],
+};
+
+const categoriesSlice = createSlice({
+    name: "categories",
+    initialState,
+    reducers: {
+        checkStatus: (state) => {
+            state.categories.push('Under Construction');
+        },
+    }
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+export default categoriesSlice.reducer;
